2c08fd46b5864037f535f127d37dc3adff4092fe
Ragoha <ragoha92@naver.com>
2024-02-20 09:09:02 +0900

    [20240220] 스프링시큐리티 수정 및 권한 테스트

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -13,5 +14,36 @@ public class IndexController {
     // 뷰리졸버 설정 : templates (prefix), .mustache (suffix) (생략가능)
     public String index(){
         return "index"; // src/main/resources/templates/index.mustache
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    // 스프링시큐리티 해당주소를 낚아챔 -> SecurityConfig 파일 생성후 작동안함.
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입완료됨!";
     }
 }
